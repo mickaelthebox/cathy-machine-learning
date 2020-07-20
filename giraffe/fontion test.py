b66dd056b955feb51d9fc5d84c35edc77ba71c9f
mickael.chau <mickael.chau@epita.fr>
2020-07-20 13:31:21 +0200

    modif moyenne

--- giraffe/fontion test.py
+++ giraffe/fontion test.py
@@ -2,13 +2,25 @@
 
 
 # Test de fontions
-def moyenne(note):
+def moyenne():
     a = False
-    note = input("entrer vos notes:,")
-    note=float
-    while a== False :
+    note = input("entrer vos notes:")
+    note = float
+    while a == False :
         note = note + i
-        i= [i+1]
-        t=t+1
+        i = [i+1]
+        t+=1
     moyenne= note/len(t)
     return moyenne
+
+
+def moyenne2(list):
+    longueur = len(list)
+    sum = 0 
+    i = 0
+    for i in range(longueur):
+        sum += list[i]
+    return sum/longueur
+
+
+print(moyenne2([5,10]))
